c0c26ebdaa3af2de7a0c85eab67624ee2410a199
bteodora <149783651+bteodora@users.noreply.github.com>
2024-03-06 17:20:41 +0100

    Create TourRepository.cs

---
+++ Repository/TourRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    public class TourRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tours.csv";
+
+        private readonly Serializer<Tour> _serializer;
+
+        private List<Tour> _tours;
+
+        public TourRepository()
+        {
+            _serializer = new Serializer<Tour>();
+            _tours = _serializer.FromCSV(FilePath);
+        }
+
+        //add za sada
+
+        public void Add(Tour tour) 
+        {
+            _tours.Add(tour);
+            _serializer.ToCSV(FilePath, _tours);
+        }
+
+    }
+}
