43a901dfab1b5c179d898f18693d708228888878
Kiss Ákos <kiss.akos02@gmail.com>
2024-03-08 10:48:43 +0100

    Class TourReservation fixed (added his own id)
    There will be maybe more changes in the future for this class

--- Model/TourReservation.cs
+++ Model/TourReservation.cs
@@ -9,25 +9,28 @@ namespace BookingApp.Model
 {
     public class TourReservation : ISerializable
     {
+        private int _id;
         private List<int> _touristIds;
         private int _tourId;
         private int _startCheckpoint;
 
         TourReservation() { }
 
-        TourReservation(int tourId, int startCheckpoint)
+        TourReservation(int id, int tourId, int startCheckpoint)
         {
+            _id = id;
             _tourId = tourId;
             _startCheckpoint = startCheckpoint;
         }
 
         public void FromCSV(string[] values)
         {
-            _tourId = Convert.ToInt32(values[0]);
-            _startCheckpoint = Convert.ToInt32(values[1]);
-            if (!string.IsNullOrEmpty(values[2]))
+            _id = Convert.ToInt32(values[0]);
+            _tourId = Convert.ToInt32(values[1]);
+            _startCheckpoint = Convert.ToInt32(values[2]);
+            if (!string.IsNullOrEmpty(values[3]))
             {
-                string touristIds = values[6];
+                string touristIds = values[3];
                 List<string> touristIdsList = touristIds.Split(',').ToList();
                 // pretvaranje sa List<string> u List<int>
                 _touristIds = touristIdsList.Select(int.Parse).ToList();
@@ -38,7 +41,7 @@ namespace BookingApp.Model
         {
             string touristIds = _touristIds != null ? string.Join(",", _touristIds) : "";
 
-            string[] csValue = { _tourId.ToString(), _startCheckpoint.ToString(), touristIds };
+            string[] csValue = { _id.ToString(), _tourId.ToString(), _startCheckpoint.ToString(), touristIds };
 
             return csValue;
         }
