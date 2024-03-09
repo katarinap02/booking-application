775341c882e700e631a8c9fa30f5c740de929071
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-03-09 18:38:06 +0100

    Implemented showing all accommodations

--- View/GuestWindows/ShowAndSearchAccommodations.xaml.cs
+++ View/GuestWindows/ShowAndSearchAccommodations.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BookingApp.DTO;
+using BookingApp.Repository;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +14,44 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using BookingApp.Observer;
+using BookingApp.Model;
 namespace BookingApp.View
 {
 
-    public partial class ShowAndSearchAccommodations : Window
+    public partial class ShowAndSearchAccommodations : Window, IObserver
     {
-        public ShowAndSearchAccommodations()
+        public ObservableCollection<AccommodationDTO> Accommodations { get; set; }
+
+        public AccommodationDTO SelectedAccommodation { get; set; }
+
+        public AccommodationRepository accommodationRepository { get; set; }    
+
+        public ShowAndSearchAccommodations(AccommodationRepository accommodationRepository)
         {
+            
+
             InitializeComponent();
+
+            Accommodations = new ObservableCollection<AccommodationDTO>();
+            this.accommodationRepository = accommodationRepository;
+            accommodationRepository.AccommodationSubject.Subscribe(this);
+            //AccommodationsDataGrid.ItemsSource = Accommodations;
+            DataContext = this;
+            Update();
+            
+
+        }
+
+        public void Update()
+        {
+            Accommodations.Clear();
+            foreach(Accommodation accommodation in accommodationRepository.GetAll())
+            {
+                
+                Accommodations.Add(new AccommodationDTO(accommodation));
+                //MessageBox.Show(Accommodations[0].Type.ToString());
+            }
         }
 
        
