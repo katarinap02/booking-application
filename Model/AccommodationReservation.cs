fd8704422bfe440537bb80fd43a69692fba69dd3
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-03-10 16:11:36 +0100

    Added a missing constructor

--- Model/AccommodationReservation.cs
+++ Model/AccommodationReservation.cs
@@ -1,6 +1,7 @@
 ﻿using BookingApp.Serializer;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,13 @@ namespace BookingApp.Model
         public DateTime EndDate { get; set; }
         public string DateRange => StartDate.ToString() + "-" + EndDate.ToString();
 
+        public Calendar Calendar { get; set; }
+
+        public AccommodationReservation()
+        {
+           
+
+        }
         public string[] ToCSV()
         {
             string[] csvValues =
