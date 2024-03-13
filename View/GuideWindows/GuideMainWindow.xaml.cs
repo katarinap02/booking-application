53bb7c6e090d3d099bc1dbb221f2905bcae46d7f
bteodora <149783651+bteodora@users.noreply.github.com>
2024-03-13 22:47:29 +0100

    Made main windows for guide nut currently editing them

---
+++ View/GuideWindows/GuideMainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.View.GuideWindows
+{
+    public partial class GuideMainWindow
+    {
+    }
+}
