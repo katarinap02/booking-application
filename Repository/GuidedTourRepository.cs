2cc05b99194c662509cc9d4288ede9b548121b80
bteodora <149783651+bteodora@users.noreply.github.com>
2024-03-13 19:52:50 +0100

    Merge branch 'GuidedTour' into development

---
+++ Repository/GuidedTourRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    public class GuidedTourRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tours.csv";
+        private readonly Serializer<GuidedTour> _serializer;
+        private List<GuidedTour> _guidedTours;
+
+        public GuidedTourRepository()
+        {
+            _serializer = new Serializer<GuidedTour>();
+            _guidedTours = _serializer.FromCSV(FilePath);
+        }
+
+        public List<GuidedTour> GetAll() {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public void Add(User guide, Tour tour) {
+            GuidedTour guidedTour = new GuidedTour(guide, tour);
+            _guidedTours.Add(guidedTour);
+            _serializer.ToCSV(FilePath, _guidedTours);
+        }
+
+    }
+}
