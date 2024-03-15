a6114e7199f3c89f2f400f7ea47c09cea070950f
bteodora <149783651+bteodora@users.noreply.github.com>
2024-03-15 09:57:02 +0100

    Implemented Main WIndow for guide without tour

--- View/GuideWindows/GuideMainWindow.xaml.cs
+++ View/GuideWindows/GuideMainWindow.xaml.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using BookingApp.DTO;
+using BookingApp.Model;
+using BookingApp.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace BookingApp.View.GuideWindows
 {
-    public partial class GuideMainWindow
+    public partial class GuideMainWindow : Window
     {
+        public List<Tour> TodaysTours;
+        // Informacije o vodicu u konstruktoru
+        public User Guide { get; set; }
+        // lista dostupnih tura
+        public List<TourDTO> TodayDTOs;
+        // tour repo -> TourDTO ???
+        private readonly TourRepository TourRepository;
+        public TourDTO SelectedTour { get; set; }
+
+        public GuideMainWindow(User user)
+        {
+            TourRepository = new TourRepository();
+            Guide = user;
+            TodaysTours = TourRepository.findToursNeedingGuide();
+            TodayDTOs = new List<TourDTO>();
+
+            foreach(Tour tour in TodaysTours)
+            {
+                TodayDTOs.Add(new TourDTO(tour));
+                MessageBox.Show((tour.Name));
+            }
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewTourWindow newTourWindow = new NewTourWindow();
+            newTourWindow.ShowDialog();
+        }
     }
 }
