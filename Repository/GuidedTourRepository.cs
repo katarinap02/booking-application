ad57bdbbacfdf2de846d81397106eac4ba7218fe
bteodora <149783651+bteodora@users.noreply.github.com>
2024-03-16 19:18:00 +0100

    Implemented new methodes in Guided tour repo

--- Repository/GuidedTourRepository.cs
+++ Repository/GuidedTourRepository.cs
@@ -10,7 +10,7 @@ namespace BookingApp.Repository
 {
     public class GuidedTourRepository
     {
-        private const string FilePath = "../../../Resources/Data/tours.csv";
+        private const string FilePath = "../../../Resources/Data/guided_tour.csv";
         private readonly Serializer<GuidedTour> _serializer;
         private List<GuidedTour> _guidedTours;
 
@@ -27,6 +27,28 @@ namespace BookingApp.Repository
         public void Add(User guide, Tour tour) {
             GuidedTour guidedTour = new GuidedTour(guide, tour);
             _guidedTours.Add(guidedTour);
+            _serializer.ToCSV(FilePath, _guidedTours);
+        }
+
+        public bool HasTourCurrently(int guide_id)
+        {
+            return _guidedTours.Find(t => t.GuideId == guide_id) != null;
+        }
+        
+        public int FindTourIdByGuide(int guide_id)
+        {
+            GuidedTour guidedTour = _guidedTours.Find(t => t.GuideId == guide_id);
+            return guidedTour.TourId;
+        }
+
+        public bool Exists(int guide_id, int tour_id) {
+            return _guidedTours.Find(t => t.GuideId == guide_id && t.TourId == tour_id) != null;
+        }
+
+        public void Remove(int guide_id, int tour_id)
+        {
+            GuidedTour guidedTour = _guidedTours.Find(t => t.GuideId == guide_id && t.TourId == tour_id);
+            _guidedTours.Remove(guidedTour);
             _serializer.ToCSV(FilePath, _guidedTours);
         }
 
