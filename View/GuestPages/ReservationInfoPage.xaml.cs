c80394358b6d63c7ae502c62d8d388d151e86d45
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-03-26 02:36:12 +0100

    Configuring ReservationInfo page

--- View/GuestPages/ReservationInfoPage.xaml.cs
+++ View/GuestPages/ReservationInfoPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BookingApp.DTO;
+using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,77 @@ namespace BookingApp.View.GuestPages
     /// </summary>
     public partial class ReservationInfoPage : Page
     {
-        public ReservationInfoPage()
+        public AccommodationDTO SelectedAccommodation { get; set; }
+        public AccommodationRepository AccommodationRepository { get; set; }
+
+        public User User { get; set; }
+        public int DayNumber { get; set; }
+        public Frame Frame { get; set; }
+
+       
+        public ReservationInfoPage(AccommodationRepository accommodationRepository, AccommodationDTO SelectedAccommodation, User user, Frame frame)
         {
             InitializeComponent();
+            this.SelectedAccommodation = SelectedAccommodation;
+            this.AccommodationRepository = accommodationRepository;
+            this.User = user;
+            this.Frame = frame;
+            bool dateIsValid;
+            bool dayNumberIsValid;
+            DataContext = this;
+          
+           
+
+           
+        }
+
+        private void Continue_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+           
+            
+            DayNumber = Convert.ToInt32(txtDayNumber.Text);
+            DateTime start = Convert.ToDateTime(txtStartDate.Text);
+            DateTime end = Convert.ToDateTime(txtEndDate.Text);
+
+           
+
+           
+
+            Frame.Content = new CalendarPage(AccommodationRepository, SelectedAccommodation, DayNumber, User, start, end, Frame);
+
+
+        }
+
+        private bool ValidateDayNumber(int dayNumber)
+        {
+            if (DayNumber < SelectedAccommodation.MinReservationDays)
+            {
+                
+                return false;
+            }
+            else
+            {
+                
+                return true;
+            }
+        }
+
+        private bool ValidateDateInputs(DateTime start, DateTime end)
+        {
+            if (start >= end)
+            {
+               
+                return false;
+            }
+            else
+            {
+               
+                return true;
+            }
+
+
         }
     }
 }
