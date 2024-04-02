2412f9b3d10421021d4ec176fb2707b440edd63d
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2024-04-02 12:31:38 +0200

    added backend superHost

--- Repository/HostRepository.cs
+++ Repository/HostRepository.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace BookingApp.Repository
 {
@@ -14,14 +15,24 @@ namespace BookingApp.Repository
         private const string FilePath = "../../../Resources/Data/hosts.csv";
 
         private readonly Serializer<Host> _serializer;
+        private readonly Serializer<AccommodationRate> _serializerRate;
 
         private List<Host> _hosts;
+
+        private List<AccommodationRate> _rates;
+        private const string FilePathRate = "../../../Resources/Data/accommodation_rate.csv";
+        
+
+        public Subject HostSubject { get; set; }
 
         public HostRepository()
         {
             _serializer = new Serializer<Host>();
+            _serializerRate = new Serializer<AccommodationRate>();
             _hosts = _serializer.FromCSV(FilePath);
-            
+            HostSubject = new Subject();
+            _rates = _serializerRate.FromCSV(FilePathRate);
+
         }
 
         public List<Host> GetAll()
@@ -35,9 +46,62 @@ namespace BookingApp.Repository
             return _hosts.FirstOrDefault(u => u.Username == username);
         }
 
-        public bool BecomeSuperHost(Host host)
+        public Host Update(Host host)
         {
-            return false;
+            _hosts = _serializer.FromCSV(FilePath);
+            Host current = _hosts.Find(a => a.Id == host.Id);
+            int index = _hosts.IndexOf(current);
+            _hosts.Remove(current);
+            _hosts.Insert(index, host);
+            _serializer.ToCSV(FilePath, _hosts);
+            HostSubject.NotifyObservers();
+            return host; //vraca proslo
         }
+
+        public void BecomeSuperHost(Host host)
+        {
+            int counter = 0;
+            double gradeSum = 0;
+            foreach(AccommodationRate rate in _rates)
+            {
+                if(rate.HostId == host.Id)
+                {
+                    counter++;
+                    gradeSum += (rate.Correctness + rate.Cleanliness) / 2;
+                }
+            }
+
+            if(counter < 10)
+            {
+                host.IsSuperHost = false;
+                Update(host);
+                return;
+            }
+
+            double average = gradeSum / Convert.ToDouble(counter);
+            if(average >= 4.5)
+            {
+                host.IsSuperHost = true; ;
+                
+            }
+            else
+            {
+                host.IsSuperHost = false;
+            }
+
+            Update(host);
+            return;
+        }
+
+        public Host GetById(int hostId)
+        {
+            foreach (Host host in _hosts)
+            {
+                if (hostId == host.Id) return host;
+            }
+
+            return null;
+        }
+
     }
 }
