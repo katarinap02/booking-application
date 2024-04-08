eaa759e83a15a04036e7b1a71854a609345e1555
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-04-08 16:07:19 +0200

    Added UserService

---
+++ Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using BookingApp.Model;
+using BookingApp.Repository;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository UserRepository;
+
+        public UserService()
+        {
+            UserRepository = new UserRepository();
+        }
+
+        public User GetByUsername(string username)
+        {
+           return UserRepository.GetByUsername(username);   
+        }
+    }
+}
