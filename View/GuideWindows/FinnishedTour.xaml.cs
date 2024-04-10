e71e494ebbaada40445fb2d0f637b8c1ac362d73
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-10 21:34:03 +0200

    Added a new window showing finnished tours
    Yet to implement bindings and button clicks

---
+++ View/GuideWindows/FinnishedTour.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BookingApp.DTO;
+using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookingApp.View.GuideWindows
+{
+    public partial class FinnishedTour: Window
+    {
+        TourRepository TourRepository;
+        User Guide {  get; set; }
+        TourViewModel SelectedTour { get; set; }
+        ObservableCollection<TourViewModel> TourViewModels;
+
+        public FinnishedTour(User guide)
+        {
+            TourRepository = new TourRepository();
+            Guide = guide;
+            SelectedTour = new TourViewModel();
+            TourViewModels = new ObservableCollection<TourViewModel>();
+            getGridData();
+        }
+
+        public void getGridData() {
+            List<Tour> tours = TourRepository.findToursToCancel(Guide.Id);
+            foreach(Tour tour in tours)
+            {
+                TourViewModels.Add(new TourViewModel(tour));
+            }
+            ToursDataGrid.ItemsSource = TourViewModels;
+        }
+
+
+    }
+}
