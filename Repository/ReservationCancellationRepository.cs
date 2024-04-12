5ca2e0208d3445d7f5482848d13932539f93df3f
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-04-12 16:28:58 +0200

    Added ReservationCancellationRepository

---
+++ Repository/ReservationCancellationRepository.cs
@@ -0,0 +1,42 @@
+﻿using BookingApp.Model;
+using BookingApp.Observer;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    public class ReservationCancellationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/reservation_cancellations.csv";
+
+        private readonly Serializer<ReservationCancellation> _serializer;
+
+        private List<ReservationCancellation> _cancellations;
+
+        public Subject CancellationSubject { get; set; }
+        public ReservationCancellationRepository()
+        {
+            _serializer = new Serializer<ReservationCancellation>();
+            _cancellations = new List<ReservationCancellation>();
+        }
+
+        public List<ReservationCancellation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public ReservationCancellation Add(ReservationCancellation cancellation)
+        {
+
+            _cancellations = _serializer.FromCSV(FilePath);
+            _cancellations.Add(cancellation);
+            _serializer.ToCSV(FilePath, _cancellations);
+            CancellationSubject.NotifyObservers();
+            return cancellation;
+        }
+    }
+}
