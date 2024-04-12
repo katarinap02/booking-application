6e7051c6cdd797e1bba98b048a9af34542dc7969
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-04-12 14:15:34 +0200

    Configured request details

---
+++ View/Guest/GuestPages/RequestDetailsPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BookingApp.Model;
+using BookingApp.View.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.GuestPages
+{
+    /// <summary>
+    /// Interaction logic for RequestDetailsPage.xaml
+    /// </summary>
+    public partial class RequestDetailsPage : Page
+    {
+        public User User { get; set; }
+        public Frame Frame { get; set; }
+
+        public DelayRequestViewModel ViewModel { get; set; }
+
+        public DelayRequestViewModel SelectedRequest { get; set; }
+        public RequestDetailsPage(DelayRequestViewModel selectedRequest, User user, Frame frame)
+        {
+            InitializeComponent();
+            User = user;
+            Frame = frame;
+            SelectedRequest = selectedRequest;
+            ViewModel = new DelayRequestViewModel(User, Frame, SelectedRequest);
+            
+            DataContext = ViewModel;
+        }
+
+        
+    }
+}
--- View/ViewModel/HostGuestViewModel/DelayRequestViewModel.cs
+++ View/ViewModel/HostGuestViewModel/DelayRequestViewModel.cs
@@ -242,10 +242,60 @@ namespace BookingApp.View.ViewModel
         public User User { get; set; }
         public Frame Frame { get; set; }
 
+        public DelayRequestViewModel SelectedRequest { get; set; }
+
+        public HostService HostService { get; set; }
+        public string HostUsername {  get; set; }
+
+        public string OldDateRange { get; set; }
+        public string NewDateRange { get; set; }   
+        public int NumberOfPeople { get; set; }
+        public int NumberOfDays { get; set; }
+
+        public string RequestHeader { get; set; }
+        public AccommodationViewModel Accommodation { get; set; }
+
         public ComboBox RequestStatusBox { get; set; }
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public DelayRequestViewModel(User user, Frame frame, DelayRequestViewModel selectedRequest)
+        {
+            User = user;
+            Frame = frame;
+            SelectedRequest = selectedRequest;
+            DelayRequestService = new DelayRequestService();
+            AccommodationService = new AccommodationService();
+            AccommodationReservationService = new AccommodationReservationService();
+            HostService = new HostService();
+            AccommodationReservation reservation = AccommodationReservationService.GetById(SelectedRequest.ReservationId);
+            Accommodation = new AccommodationViewModel(AccommodationService.GetById(reservation.AccommodationId));
+            HostUsername = HostService.GetById(SelectedRequest.HostId).Username;
+            OldDateRange = SelectedRequest.EndLastDate.ToString("MM/dd/yyyy") + " - " + SelectedRequest.StartLastDate.ToString("MM/dd/yyyy");
+            NewDateRange = SelectedRequest.EndDate.ToString("MM/dd/yyyy") + " - " + SelectedRequest.StartDate.ToString("MM/dd/yyyy");
+            NumberOfPeople = reservation.NumberOfPeople;
+            Comment = SelectedRequest.Comment;
+            NumberOfDays = (reservation.EndDate - reservation.StartDate).Days + 1;
+            RequestHeader = CreateRequestHeader(SelectedRequest);
+
+
+
+        }
+
+        private string? CreateRequestHeader(DelayRequestViewModel selectedRequest)
+        {
+            if (selectedRequest.Status == RequestStatus.PENDING)
+            {
+                Comment = "Waiting for host's response...";
+                return "Your request is pending.";
+            }
+                
+            else if (selectedRequest.Status == RequestStatus.APPROVED)
+                return "Your request is approved.";
+            else
+                return "Your request is rejected";
         }
 
         public DelayRequestViewModel(User user, Frame frame, RequestsPage page)
