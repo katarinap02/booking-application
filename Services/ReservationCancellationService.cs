92e86db301ae9cef0a7e215cb3bcbf3f19b4cb00
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-04-12 16:31:16 +0200

    Added ReservationCancellationService

---
+++ Services/ReservationCancellationService.cs
@@ -0,0 +1,30 @@
+﻿using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Services
+{
+    public class ReservationCancellationService
+    {
+        private readonly ReservationCancellationRepository ReservationCancellationRepository;
+
+        public ReservationCancellationService()
+        {
+            ReservationCancellationRepository = new ReservationCancellationRepository();
+        }
+
+        public List<ReservationCancellation> GetAll()
+        {
+            return ReservationCancellationRepository.GetAll();
+        }
+
+        public ReservationCancellation Add(ReservationCancellation cancellation)
+        {
+            return ReservationCancellationRepository.Add(cancellation);
+        }
+    }
+}
