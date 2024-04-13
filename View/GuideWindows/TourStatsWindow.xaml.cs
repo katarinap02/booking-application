a004da26d28064475fadfc6da4436b5d5fdc6303
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-13 18:20:33 +0200

    Tried implementing new window for statistics

---
+++ View/GuideWindows/TourStatsWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using LiveCharts;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+
+namespace BookingApp.View.GuideWindows
+{
+    public partial class TourStatsWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ChartValues<double> _ageStatistics;
+        public ChartValues<double> AgeStatistics
+        {
+            get { return _ageStatistics; }
+            set
+            {
+                _ageStatistics = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AgeStatistics)));
+            }
+        }
+
+        public TourStatsWindow(int tourId)
+        {
+            InitializeComponent();
+            UpdateAgeStatistics(tourId);
+            DataContext = this;
+        }
+
+        private void UpdateAgeStatistics(int tourId)
+        {
+            List<int> ageCounts = GetAgeStatistic(tourId);
+
+            AgeStatistics = new ChartValues<double>
+            {
+                ageCounts[0],
+                ageCounts[1],
+                ageCounts[2]
+            };
+        }
+
+        private List<int> GetAgeStatistic(int tourId)
+        {
+            return new List<int> { 10, 20, 5 }; // Placeholder data, replace with actual statistics
+        }
+    }
+}
