f8a2378c2f8bc4c5f095e251a74b0886119b384f
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-13 19:45:55 +0200

    Started changing the window logic depending on reviews

--- View/GuideWindows/ReviewsWindow.xaml.cs
+++ View/GuideWindows/ReviewsWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BookingApp.Repository;
+using BookingApp.Services;
+using BookingApp.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,21 @@ namespace BookingApp.View.GuideWindows
 {
     public partial class ReviewsWindow: Window 
     {
+
+        private readonly GuideRateService _guideRateService;
+
+        public ObservableCollection<GuideRateViewModel> guideRateViewModels { get; set; }   
+
+        public ReviewsWindow(int tour_id)
+        {
+            _guideRateService = new GuideRateService();
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void getData(int tour_id) {
+            
+        }
+
     }
 }
