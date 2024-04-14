5a5fac236cabb84c79e5fe4a4afe0c6866c46cc0
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-04-14 21:39:29 +0200

    Added Calculating average accommodation rate

--- Services/RateServices/AccommodationRateService.cs
+++ Services/RateServices/AccommodationRateService.cs
@@ -1,21 +1,24 @@
 ﻿using BookingApp.Model;
 using BookingApp.Repository;
 using BookingApp.Serializer;
+using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BookingApp.Services
 {
     public class AccommodationRateService
     {
         private readonly AccommodationRateRepository AccommodationRateRepository;
-
+        private AccommodationReservationService AccommodationReservationService;
         public AccommodationRateService()
         {
             AccommodationRateRepository = new AccommodationRateRepository();
+           
         }
 
         public List<AccommodationRate> GetAll()
@@ -26,6 +29,38 @@ namespace BookingApp.Services
         public AccommodationRate Add(AccommodationRate rate)
         {
             return AccommodationRateRepository.Add(rate);
+        }
+
+        public double GetAverageRate(int accommodationId)
+        {
+            AccommodationReservationService = new AccommodationReservationService();
+            double sum = 0;
+            double counter = 0;
+            foreach (AccommodationReservation reservation in AccommodationReservationService.GetAll())
+            {
+                foreach (AccommodationRate rate in AccommodationRateRepository.GetAll())
+                {
+                    if (rate.ReservationId == reservation.Id)
+                    {
+                        if (reservation.AccommodationId == accommodationId)
+                        {
+                            sum += rate.Correctness + rate.Cleanliness;
+                            counter += 2;
+
+                        }
+                    }
+                }
+            }
+
+            double average = sum / counter;
+            
+            if(average > 0)
+                return average;
+            else return 0;
+
+
+                
+                    
         }
 
         
--- View/ViewModel/HostGuestViewModel/AccommodationViewModel.cs
+++ View/ViewModel/HostGuestViewModel/AccommodationViewModel.cs
@@ -257,7 +257,8 @@ namespace BookingApp.View.ViewModel
         public string FirstPicture => "../" + OnePicture;
        
 
-     
+        private AccommodationRateService accommodationRateService = new AccommodationRateService();
+     //   public string Rate => accommodationRateService.GetAverageRate(Id).ToString();
 
         public AccommodationViewModel(Accommodation accommodation)
         {
