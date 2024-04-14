b13b4c2e1aaaf2ebf4ca56a9b8ece9b082bd853c
Natasa Radmilovic <nata.radmilovic@gmail.com>
2024-04-14 22:06:09 +0200

    added check

--- View/ViewModel/HostGuestViewModel/GuestViewModels/ReservationsToRateViewModel.cs
+++ View/ViewModel/HostGuestViewModel/GuestViewModels/ReservationsToRateViewModel.cs
@@ -53,7 +53,7 @@ namespace BookingApp.View.ViewModel
         private bool IsBeforeFiveDays(AccommodationReservation reservation)
         {
             int daysPassed = (DateTime.Now - reservation.EndDate).Days;
-            if (daysPassed <= 5)
+            if (daysPassed <= 5 && daysPassed > 0)
                 return true;
             else
                 return false;
