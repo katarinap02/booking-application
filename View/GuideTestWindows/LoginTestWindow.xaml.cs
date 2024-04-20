9a6191444eb11f92899027589a994d91793c8a2b
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-20 02:37:38 +0200

    New LOG IN window, more beautiful and aestethic

---
+++ View/GuideTestWindows/LoginTestWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BookingApp.View.GuideTestWindows
+{
+    public partial class LoginTestWindow: Window
+    {
+        public LoginTestWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimise_Buton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Close_Buton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
