79454235bc43c461c8bb296924790fa431bf099d
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-21 18:47:58 +0200

    Animation tests

---
+++ View/GuideTestWindows/RandomTest.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.GuideTestWindows
+{
+    public partial class RandomTest : Window
+    {
+        public RandomTest()
+        {
+            InitializeComponent();
+            //AnimatePaperCircle();
+        }
+
+        
+    }
+}
