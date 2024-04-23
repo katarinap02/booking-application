eabe8dd532d661725a4923b6b3a8c575dd6ef4e1
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-04-23 08:37:22 +0200

    Added GuestService

---
+++ Application/Services/FeatureServices/GuestService.cs
@@ -0,0 +1,35 @@
+﻿using BookingApp.Domain.Model.Features;
+using BookingApp.Domain.RepositoryInterfaces.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Application.Services.FeatureServices
+{
+    public class GuestService
+    {
+        private readonly IGuestRepository GuestRepository;
+
+        public GuestService(IGuestRepository guestRepository)
+        {
+            GuestRepository = guestRepository;
+        }
+
+        public Guest GetById(int id)
+        {
+            return GuestRepository.GetById(id);
+        }
+
+        public List<Guest> GetAll()
+        {
+            return GuestRepository.GetAll();
+        }
+
+        public Guest Update(Guest guest)
+        {
+            return GuestRepository.Update(guest);
+        }
+    }
+}
