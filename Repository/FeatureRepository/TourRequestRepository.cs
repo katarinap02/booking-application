16e826960f033113f430bfd4295bf3bb9f89f2d5
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-26 18:47:19 +0200

    Create TourRequestRepository.cs

---
+++ Repository/FeatureRepository/TourRequestRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookingApp.Domain.Model.Features;
+using BookingApp.Domain.Model.Reservations;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository.FeatureRepository
+{
+    public class TourRequestRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tour_requests.csv";
+        private readonly Serializer<TourRequest> _serializer;
+        private List<TourRequest> _tourRequests;
+        
+        public TourRequestRepository()
+        {
+            _serializer = new Serializer<TourRequest>();
+            _tourRequests = _serializer.FromCSV(FilePath);
+        }
+    }
+}
+
