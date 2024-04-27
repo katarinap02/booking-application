076e8e6c5fd695031a87534492b0a252db46c6d1
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-27 22:12:44 +0200

    Changed Reviews window to follow MVVM pattern

--- WPF/View/GuideWindows/ReviewsWindow.xaml.cs
+++ WPF/View/GuideWindows/ReviewsWindow.xaml.cs
@@ -4,6 +4,7 @@ using BookingApp.Domain.RepositoryInterfaces.Rates;
 using BookingApp.Domain.RepositoryInterfaces.Reservations;
 using BookingApp.Repository;
 using BookingApp.WPF.ViewModel.GuideTouristViewModel;
+using BookingApp.WPF.ViewModel.GuideViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -17,54 +18,16 @@ namespace BookingApp.View.GuideWindows
     public partial class ReviewsWindow: Window 
     {
 
-        private readonly GuideRateService _guideRateService;
-        private readonly TourParticipantService _tourParticipantService;
-
-        public ObservableCollection<GuideRateViewModel> guideRateViewModels { get; set; }   
-        public GuideRateViewModel selectedRate { get; set; }
+        
+        public ReviewsWindowViewModel reviewsWindowViewModel { get; set; }
 
         public ReviewsWindow(int tour_id)
         {
-            _guideRateService = new GuideRateService(Injector.Injector.CreateInstance<IGuideRateRepository>()); 
-             _tourParticipantService = new TourParticipantService(Injector.Injector.CreateInstance<ITourParticipantRepository>());
             InitializeComponent();
-            DataContext = this;
-            getData(tour_id);
+            reviewsWindowViewModel = new ReviewsWindowViewModel(tour_id);
+            DataContext = reviewsWindowViewModel;
         }
 
-        private void getData(int tour_id) {
-            guideRateViewModels = new ObservableCollection<GuideRateViewModel>();
-            foreach(GuideRateViewModel rate in _guideRateService.getRatesByTour(tour_id))
-            {
-                guideRateViewModels.Add(rate);
-            }
-        }
-
-        private void Invalid_Click(object sender, RoutedEventArgs e)
-        {
-            if(selectedRate == null)
-            {
-                MessageBox.Show("PLease select a review in order to mark it as invalid");
-            }
-            else
-            {
-                _guideRateService.markAsInvalid(selectedRate.Id);
-                MessageBox.Show("Review marked as invalid", "Reviews Notification");
-            }
-        }
-
-        private void Checkpoint_Click(object sender, RoutedEventArgs e)
-        {
-            if (selectedRate == null)
-            {
-                MessageBox.Show("PLease select a review");
-            }
-            else
-            {
-                int checkpoint = _tourParticipantService.getjoinedCheckpoint(selectedRate.TourId);
-                MessageBox.Show("Joined checkpoint number: " + checkpoint.ToString());
-            }
-            
-        }
+        
     }
 }
