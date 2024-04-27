968afa35e2e3ce540383f8eea4e4a3de78056e96
bteodora <149783651+bteodora@users.noreply.github.com>
2024-04-27 22:11:50 +0200

    Changed cancelling tour to follow MVVM pattern

--- WPF/View/GuideWindows/CancelTourWindow.xaml.cs
+++ WPF/View/GuideWindows/CancelTourWindow.xaml.cs
@@ -1,61 +1,27 @@
-﻿using BookingApp.Repository;
-using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-
-using BookingApp.Application.Services;
-using BookingApp.Domain.Model.Features;
 using BookingApp.Application.Services.FeatureServices;
+using BookingApp.Domain.Model.Features;
 using BookingApp.Domain.RepositoryInterfaces.Features;
 using BookingApp.WPF.ViewModel.GuideTouristViewModel;
+using System.Collections.Generic;
+using BookingApp.WPF.ViewModel.HostGuestViewModel.GuestViewModels;
+using BookingApp.WPF.ViewModel.GuideViewModel;
 
 namespace BookingApp.View.GuideWindows
 {
-    public partial class CancelTourWindow: Window
+    public partial class CancelTourWindow : Window
     {
-        private readonly TourService tourService;
-        public User Guide { get; set; }
-        public TourViewModel SelectedTour { get; set; }
-        public ObservableCollection<TourViewModel> TourViewModels;
+        
+
+        public CancelTourWindowViewModel CancellationViewModel { get; set; }
 
         public CancelTourWindow(User guide)
         {
             InitializeComponent();
-            DataContext = this;
-            tourService = new TourService(Injector.Injector.CreateInstance<ITourRepository>());
-            Guide = guide;
-            TourViewModels = new ObservableCollection<TourViewModel>();
-            
-            getGridData();
-        }
-
-        public void getGridData()
-        {
-            TourViewModels.Clear();
-            List<Tour> tours = tourService.findToursToCancel(Guide.Id);
-            foreach (Tour tour in tours)
-            {
-                TourViewModels.Add(new TourViewModel(tour));
-            }
-            ToursDataGrid.ItemsSource = TourViewModels;
-        }
-
-        private void CancelTour_Click(object sender, RoutedEventArgs e)
-        {
-            if(SelectedTour  != null)
-            {
-                MessageBox.Show(SelectedTour.Id.ToString());
-                tourService.cancelTour(SelectedTour.Id, Guide.Id);
-                getGridData();
-            }
-            else
-            {
-                MessageBox.Show("Please select a tour to cancel!");
-            }
+            CancellationViewModel = new CancelTourWindowViewModel(guide);
+            DataContext = CancellationViewModel;
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
@@ -64,3 +30,4 @@ namespace BookingApp.View.GuideWindows
         }
     }
 }
+
