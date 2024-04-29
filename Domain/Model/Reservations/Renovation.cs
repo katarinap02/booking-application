53958011455cfb87815d424fe71766eb550e79dc
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2024-04-29 21:31:59 +0200

    added new classes and services

---
+++ Domain/Model/Reservations/Renovation.cs
@@ -0,0 +1,62 @@
+﻿using BookingApp.Domain.Model.Features;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ISerializable = BookingApp.Serializer.ISerializable;
+
+namespace BookingApp.Domain.Model.Reservations
+{
+    public class Renovation : ISerializable
+    {
+        public int Id;
+
+        public int AccommodationId;
+
+        public DateTime StartDate;
+
+        public DateTime EndDate;
+
+        public string DateRange => StartDate.ToString() + "-" + EndDate.ToString();
+
+        public int Duration;
+
+        public Renovation() { }
+
+        public Renovation(int accommodationId, DateTime startDate, DateTime endDate, int duration)
+        {
+            AccommodationId = accommodationId;
+            StartDate = startDate;
+            EndDate = endDate;
+            Duration = duration;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            AccommodationId = Convert.ToInt32(values[1]);
+            string[] dateParts = values[2].Split('-');
+            StartDate = DateTime.Parse(dateParts[0]);
+            EndDate = DateTime.Parse(dateParts[1]);
+            Duration = Convert.ToInt32(values[4]);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                AccommodationId.ToString(),
+                DateRange,
+                Duration.ToString()
+
+            };
+            return csvValues;
+        }
+    }
+}
---
+++ Repository/ReservationRepository/RenovationRepository.cs
@@ -0,0 +1,63 @@
+﻿using BookingApp.Domain.Model.Reservations;
+using BookingApp.Domain.RepositoryInterfaces.Reservations;
+using BookingApp.Observer;
+using BookingApp.Serializer;
+using BookingApp.WPF.ViewModel.HostGuestViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository.ReservationRepository
+{
+    public class RenovationRepository : IRenovationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/renovation.csv";
+        private readonly Serializer<Renovation> _serializer;
+        private List<Renovation> _renovations;
+
+        public Subject RenovationSubject { get; set; }
+
+        public RenovationRepository()
+        {
+            _serializer = new Serializer<Renovation>();
+            _renovations = new List<Renovation>();  
+            RenovationSubject = new Subject();
+        }
+
+        public List<Renovation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Renovation Add(Renovation reservation)
+        {
+            reservation.Id = NextId();
+            _renovations = _serializer.FromCSV(FilePath);
+            _renovations.Add(reservation);
+            _serializer.ToCSV(FilePath, _renovations);
+            RenovationSubject.NotifyObservers();
+            return reservation;
+        }
+
+        public int NextId()
+        {
+            _renovations = _serializer.FromCSV(FilePath);
+            if (_renovations.Count < 1)
+                return 1;
+
+            return _renovations.Max(a => a.Id) + 1;
+        }
+
+        public void Delete(Renovation selectedReservation)
+        {
+            _renovations = _serializer.FromCSV(FilePath);
+            Renovation found = _renovations.Find(r => r.Id == selectedReservation.Id);
+            if (found != null)
+            _renovations.Remove(found);
+            _serializer.ToCSV(FilePath, _renovations);
+            RenovationSubject.NotifyObservers();
+        }
+    }
+}
