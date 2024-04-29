378574e037748a43598fa847e68b42358189eb59
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2024-04-29 21:47:53 +0200

    added new viewmodel

---
+++ WPF/ViewModel/HostGuestViewModel/RenovationViewModel.cs
@@ -0,0 +1,105 @@
+﻿using BookingApp.Application.Services.FeatureServices;
+using BookingApp.Domain.RepositoryInterfaces.Features;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.WPF.ViewModel.HostGuestViewModel
+{
+    public class RenovationViewModel : INotifyPropertyChanged
+    {
+       
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+
+
+        private int accommodationId;
+        public int AccommodationId
+        {
+            get { return accommodationId; }
+            set
+            {
+                if (accommodationId != value)
+                {
+
+                    accommodationId = value;
+                    OnPropertyChanged("AccommodationId");
+                }
+            }
+        }
+
+        private DateTime startDate;
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set
+            {
+                if (startDate != value)
+                {
+
+                    startDate = value;
+                    OnPropertyChanged("StartDate");
+                }
+            }
+        }
+
+        private DateTime endDate;
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set
+            {
+                if (endDate != value)
+                {
+
+                    endDate = value;
+                    OnPropertyChanged("EndDate");
+                }
+            }
+        }
+
+        private int duration;
+        public int uration
+        {
+            get { return duration; }
+            set
+            {
+                if (duration != value)
+                {
+                    duration = value;
+                    OnPropertyChanged("Duration");
+                }
+            }
+        }
+
+        private AccommodationService accommodationService = new AccommodationService(Injector.Injector.CreateInstance<IAccommodationRepository>());
+
+        public string DateString => StartDate.ToString("MM/dd/yyyy") + " -> " + EndDate.ToString("MM/dd/yyyy");
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+
+
+}
