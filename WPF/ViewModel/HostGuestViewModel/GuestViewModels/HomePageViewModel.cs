47062199485be27b0c7759a729c404d7bf3066e5
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-05-08 18:49:02 +0200

    HCI - Added commands to HomePage

--- WPF/ViewModel/HostGuestViewModel/GuestViewModels/HomePageViewModel.cs
+++ WPF/ViewModel/HostGuestViewModel/GuestViewModels/HomePageViewModel.cs
@@ -3,6 +3,9 @@ using BookingApp.Domain.Model.Features;
 using BookingApp.Domain.RepositoryInterfaces.Features;
 using BookingApp.Domain.RepositoryInterfaces.Reservations;
 using BookingApp.Repository;
+using BookingApp.View.GuestPages;
+using BookingApp.WPF.View.Guest.GuestPages;
+using BookingApp.WPF.ViewModel.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +21,9 @@ namespace BookingApp.WPF.ViewModel.HostGuestViewModel.GuestViewModels
         public Frame Frame { get; set; }
         public Guest Guest { get; set; }
         public GuestService GuestService { get; set; }
+        public GuestICommand ReserveCommand { get; set; }
+        public GuestICommand AboutCommand { get; set; }
+        public GuestICommand HelpCommand { get; set; }
         public HomePageViewModel(User user, Frame frame) { 
             
             User = user;
@@ -25,8 +31,27 @@ namespace BookingApp.WPF.ViewModel.HostGuestViewModel.GuestViewModels
             GuestService = new GuestService(Injector.Injector.CreateInstance<IGuestRepository>(), Injector.Injector.CreateInstance<IAccommodationReservationRepository>(), Injector.Injector.CreateInstance<IDelayRequestRepository>());
             Guest = GuestService.GetById(User.Id);
             GuestService.CalculateGuestStats(Guest);
-        
-        
+            ReserveCommand = new GuestICommand(OnReserve);
+            AboutCommand = new GuestICommand(OnAbout);
+            HelpCommand = new GuestICommand(OnHelp);
+
+
+        }
+
+        private void OnAbout()
+        {
+            Frame.Content = new AboutPage();
+        }
+
+        private void OnHelp()
+        {
+
+        }
+
+        private void OnReserve()
+        {
+
+            Frame.Content = new AccommodationsPage(User, Frame);
         }
     }
 }
