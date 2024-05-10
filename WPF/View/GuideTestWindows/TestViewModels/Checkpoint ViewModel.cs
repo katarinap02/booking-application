2e2dc457b52d927c713b01b38c2781bed14a3384
bteodora <149783651+bteodora@users.noreply.github.com>
2024-05-11 00:16:54 +0200

    Create Checkpoint ViewModel.cs

---
+++ WPF/View/GuideTestWindows/TestViewModels/Checkpoint ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BookingApp.WPF.View.GuideTestWindows.TestViewModels
+{
+    public class CheckpointViewModel : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; NotifyPropertyChanged(nameof(Name)); }
+        }
+
+        private Brush indicatorColor;
+        public Brush IndicatorColor
+        {
+            get { return indicatorColor; }
+            set { indicatorColor = value; NotifyPropertyChanged(nameof(IndicatorColor)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
