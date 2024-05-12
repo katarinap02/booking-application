749121c01d0e0fed4ebf768f57279d5a1d17393b
Kiss Ákos <kiss.akos02@gmail.com>
2024-05-13 01:12:57 +0200

    Tourist main winow buttons changed to command

--- WPF/View/TouristWindows/TouristWindow.xaml.cs
+++ WPF/View/TouristWindows/TouristWindow.xaml.cs
@@ -56,30 +56,6 @@ namespace BookingApp.View.TouristWindows
             base.OnClosed(e);
             Messenger.Default.Unregister(this);
         }
-
-        private void AllToursButton_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new AllToursPage(Tour.getUserId(Tour.UserName));
-        }
-
-        private void MyToursButton_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new MyToursPage(Tour.SelectedTour, Tour.getUserId(Tour.UserName));
-        }
-
-        private void EndedToursButton_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new EndedToursPage(Tour.getUserId(Tour.UserName));
-        }
-        private void RequestedToursButton_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new RequestedToursPage(Tour.getUserId(Tour.UserName));
-        }
-        private void VouchersButton_Click(object sender, RoutedEventArgs e)
-        {
-            MainFrame.Content = new VouchersPage(Tour.getUserId(Tour.UserName));
-        }
-
         private void AllTours_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
