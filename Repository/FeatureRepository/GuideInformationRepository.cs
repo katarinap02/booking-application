d68bc9d2b0be3e17571eeeb344842d65979ef1c0
bteodora <149783651+bteodora@users.noreply.github.com>
2024-05-12 02:22:09 +0200

    Implemented a new class called Guide Information used for getting all the info about the guide

---
+++ Repository/FeatureRepository/GuideInformationRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookingApp.Domain.Model.Features;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository.FeatureRepository
+{
+    class GuideInformationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/guide_info.csv";
+        private readonly Serializer<GuideInformation> _serializer;
+        private List<GuideInformation> _guideInformation;
+
+        public GuideInformationRepository()
+        {
+            _serializer = new Serializer<GuideInformation>();
+            _guideInformation = _serializer.FromCSV(FilePath);
+        }
+
+        public List<GuideInformation> GetAll()
+        {
+            _guideInformation = _serializer.FromCSV(FilePath);
+            return _guideInformation;
+        }
+
+        public GuideInformation GetByGuideId(int id)
+        {
+            return _guideInformation.Find(x => x.UserId ==  id);
+        }
+    }
+}
