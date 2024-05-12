dc446ceb55112b51484c165eec04a8a3ecd847fa
bteodora <149783651+bteodora@users.noreply.github.com>
2024-05-12 20:36:05 +0200

    Changed location of the request window and added some methodes

--- WPF/View/GuideWindows/RequestTest.xaml.cs
+++ WPF/View/GuideWindows/RequestTest.xaml.cs
@@ -5,39 +5,47 @@ using BookingApp.WPF.ViewModel.GuideTouristViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace BookingApp.WPF.View.GuideTestWindows
+namespace BookingApp.WPF.View.GuideWindows
 {
     public partial class RequestTest: Window
     {
         private readonly TourRequestService _tourRequestService;
         public ObservableCollection<TourRequestDTOViewModel> tourRequestViewModels { get; set; }
+        public TourRequestDTOViewModel SearchCriteria { get; set; }
+
         public RequestTest()
         {
-            _tourRequestService = new TourRequestService(Injector.Injector.CreateInstance<ITourRequestRepository>());
-            InitializeComponent();
             DataContext = this;
+            SearchCriteria = new TourRequestDTOViewModel(DateTime.MaxValue);
+            _tourRequestService = new TourRequestService(Injector.Injector.CreateInstance<ITourRequestRepository>());            
+            DataContext = this;
+            tourRequestViewModels = new ObservableCollection<TourRequestDTOViewModel>();
             tourRequestViewModels = getData();
+            InitializeComponent();
         }
 
         public ObservableCollection<TourRequestDTOViewModel> getData() 
-        {
-            List<int> ints = new List<int>();
-            DateTime dateTime = new DateTime(2023, 1, 1);
-            DateTime dateTime2 = new DateTime(2024, 6, 30);
-            TourRequest searchCriteria = new TourRequest("desc", "gli", ints, dateTime, dateTime2, "", "");
-            List<TourRequest> tourRequests = _tourRequestService.filterRequests(searchCriteria); // kada stavim neku drugu funkciju radi savrseno 
+        {            
+            List<TourRequest> tourRequests = _tourRequestService.filterRequests(SearchCriteria.ToTourRequest()); 
+            
             ObservableCollection<TourRequestDTOViewModel> viewModels = new ObservableCollection<TourRequestDTOViewModel>();
             foreach (var tourRequest in tourRequests)
             {
                 viewModels.Add(new TourRequestDTOViewModel(tourRequest));
-            }            
+            }
             return viewModels;
         }
 
+        private void Testic(object sender, RoutedEventArgs e)
+        {
+            SearchCriteria.StartDate = DateTime.MinValue;
+            SearchCriteria.EndDate = DateTime.MaxValue;
+            SearchCriteria.Language = "";
+            SearchCriteria.City = "";
+            SearchCriteria.Country = "";
+            // no of tourists
+        }
     }
 }
